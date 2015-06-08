d5832165045ba05732438d005a0d0a499e846be5
wiiam <wiiam24@gmail.com>
2015-06-09 00:01:44 +1200

    Changed rules

--- Rules.java
+++ Rules.java
@@ -12,11 +12,12 @@ public class Rules implements Module {
 		if(m.isBotCommand()){
 			if(m.getBotCommand().equals(command)){
 				System.out.println(".rules");
-				String[] outputs  = new String[4];
+				String[] outputs  = new String[5];
 				outputs[0] = "PRIVMSG " + target + " :Rule 1: No Spamming";
 				outputs[1] = "PRIVMSG " + target + " :Rule 2: Tag all NSFW links";
 				outputs[2] = "PRIVMSG " + target + " :Rule 3: Bots must follow IBIP, this can be found at https://github.com/Teknikode/IBIP";
 				outputs[3] = "PRIVMSG " + target + " :Rule 4: installgen2 is a cuck";
+				outputs[4] = "PRIVMSG " + target + " :Rule 5: No furries";
 				return outputs;
 			}
 		}
